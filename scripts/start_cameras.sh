d0dc24fad5845704598d7e987cc1a32f7967d0aa
twbadmin <twbadmin@twbserver>
2017-10-30 11:58:44 +0100

    created tracker and sense package. renamed tracker_artoolkit5 to artoolkit5 and renamed sense driver package to cvb.

--- scripts/start_cameras.sh
+++ scripts/start_cameras.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-roslaunch sense example.launch CAM:=1
+roslaunch cvb example.launch CAM:=1
 sleep 5
-roslaunch sense example.launch CAM:=2
+roslaunch cvb example.launch CAM:=2
 sleep 5
-roslaunch sense example.launch CAM:=3
+roslaunch cvb example.launch CAM:=3
 sleep 5
-roslaunch sense example.launch CAM:=4
+roslaunch cvb example.launch CAM:=4
