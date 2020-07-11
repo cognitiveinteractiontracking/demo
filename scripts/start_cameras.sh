39031fc5c893bd42f0ebd932ffdeef20c408927f
Timo Korthals <tkorthals@cit-ec.uni-bielefeld.de>
2020-07-11 23:10:28 +0200

    Cleanup
    Signed-off-by: Timo Korthals <tkorthals@cit-ec.uni-bielefeld.de>

--- scripts/start_cameras.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-roslaunch cvb example.launch CAM:=1
-sleep 5
-roslaunch cvb example.launch CAM:=2
-sleep 5
-roslaunch cvb example.launch CAM:=3
-sleep 5
-roslaunch cvb example.launch CAM:=4
